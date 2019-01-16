60021b790996ddee011c9b38b7706c95212a4bba
Geoff <myers.geoffrey@gmail.com>
2019-01-15 21:51:33 -0500

    define raster class

--- ClassRoster-Myers/roster.cpp
+++ ClassRoster-Myers/roster.cpp
@@ -1,4 +1,4 @@
-/*#include "roster.h"
+#include "roster.h"
 
 
 Roster::Roster()
@@ -9,4 +9,13 @@ Roster::Roster()
 Roster::~Roster()
 {
 }
-*/
+
+
+const string studentData[] =
+{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+"A5,Geoffrey,Myers,gmyer12@wgu.edu,39,5,10,25,SOFTWARE" }
+
+
--- ClassRoster-Myers/roster.h
+++ ClassRoster-Myers/roster.h
@@ -1,8 +1,28 @@
 #pragma once
+#include "networkStudent.h"
+#include "securityStudent.h"
+#include "softwareStudent.h"
+#include <vector>
+
+
 class Roster
 {
 public:
 	Roster();
 	~Roster();
+
+   void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, Degree program);
+   void remove(string studentID);
+
+   void printAll();
+
+   void printDaysInCourse(string studentID);
+
+   void printInvalidEmails();
+
+   void printByDegreeProgram(int degreeProgram);
+
+private:
+   vector<Student*> classRoster;
 };
 
